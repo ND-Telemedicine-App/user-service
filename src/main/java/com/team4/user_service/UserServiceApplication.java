a3c6f4759f0a101970710560ecfaedcc30a3f0b0
Hoang Linh <hlinh.jobs@gmail.com>
2022-09-19 14:21:52 +1000

    create mock data

--- src/main/java/com/team4/user_service/UserServiceApplication.java
+++ src/main/java/com/team4/user_service/UserServiceApplication.java
@@ -19,15 +19,19 @@ public class UserServiceApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception
 	{
-		User newUser = new User(1L , "DOCTOR", "doctor123@gmail.com", "password", "John Dang", "assets/images/hansooyoung.jpg", "123 Abc Street", "0123456789", "Male", new Date(76, 12,21), "","","",
-		"Dr John has over 14 years’ experience as a GP.", "Surgery");
-		User newUser1 = new User(2L , "DOCTOR", "doctor456@gmail.com", "password", "Alicia Nguyen", "assets/images/Junho.png", "123 Abc Street", "0123456789", "Female", new Date(76, 12,21), "","","",
-				"Dr Alicia has over 14 years’ experience as a Ophthalmologists.", "Ophthalmologists" );
-		User newUser2 = new User(3L , "DOCTOR", "doctor789@gmail.com", "password", "Ben Tran", "assets/images/kieran.jpg", "123 Abc Street", "0123456789", "Male", new Date(76, 12,21), "","","",
-				"Dr John has over 14 years’ experience as a Radiologists", "Radiologists");
+		User newUser = new User(1L , "DOCTOR", "doctor123@gmail.com", "password", "John Dang", "assets/images/doctor_ava_1.jpeg", "123 Abc Street", "0123456789", "Male", new Date(76, 12,21), "","","",
+		"Dr John has over 14 years experience as a GP.", "Surgery");
+		User newUser1 = new User(2L , "DOCTOR", "doctor456@gmail.com", "password", "Alicia Nguyen", "assets/images/doctor_ava_2.jpeg", "123 Abc Street", "0123456789", "Female", new Date(76, 12,21), "","","",
+				"Dr Alicia has over 14 years experience as a Ophthalmologists.", "Ophthalmologists" );
+		User newUser2 = new User(3L , "DOCTOR", "doctor789@gmail.com", "password", "Ben Tran", "assets/images/doctor_ava_3.jpeg", "123 Abc Street", "0123456789", "Male", new Date(76, 12,21), "","","",
+				"Dr John has over 14 years experience as a Radiologists", "Radiologists");
+		User newUser3 = new User(4L , "PATIENT", "luke@gmail.com", "password", "Luke Nguyen", "assets/images/patient.jpeg", "123 Elizabeth St, Melbourne, VIC 3000", "0413939449", "Male", new Date(76, 12,21), "Peanut Butter, Aspirin, Dairy, Penicillin, Tree nuts","Major depression, Diabetes type 2","Citalopram, Insulin",
+				"", "");
+
 		userRepository.save(newUser);
 		userRepository.save(newUser1);
 		userRepository.save(newUser2);
+		userRepository.save(newUser3);
 	}
 
 }
